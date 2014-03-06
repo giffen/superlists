f447133d559d0f1192aa6db13f15c8bc2823e1c6
Andy Giffen <giffen@gmail.com>
2014-03-05 18:36:12 -0800

    New URL, view and template to display list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		# Edit has heard about a cool new online to-do app. She goes
@@ -37,11 +42,14 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 		edith_list_url = self.browser.current_url
 		self.assertRegexpMatches(edith_list_url, 'lists/.+') 
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		
-		# There is still a text box inviting her to add another item, She
-		# Enters: "Use peacock feathers to make a fly"
-		self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very
+		# methodical)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now hows both items on her list
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
