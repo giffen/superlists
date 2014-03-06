fd6223bc7144b082f8b06d441840c1ef12644d6d
Andy Giffen <giffen@gmail.com>
2014-03-05 18:02:18 -0800

    Added functional tests for having a unique URL for multiple lists.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -34,9 +35,8 @@ class NewVisitorTest(LiveServerTestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# 1: Buy peacock feathers, as an item in a to-do list	
 		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
+		edith_list_url = self.browser.current_url
+		self.assertRegexpMatches(edith_list_url, 'lists/.+') 
 		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		
 		# There is still a text box inviting her to add another item, She
@@ -44,12 +44,36 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
 
 		# The page updates again, and now hows both items on her list
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		# Edith wonders whether the site will remember her list.  Then she sees
-		# that the site has generated a unique URL for her -- there is some
-		# explanatory text to that effect.
-		self.fail('Finish the test!')
+		# Now a new user, Francis, comes along to the site.
 
-		# She visits the URL -- her to-do list is stiil there.
+		## We use a new browser session to make sure the no information
+		##  of Edith's is coming through from cookies etc
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# Francis visits the home page. There is no sign of Edith's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		# Francis starts a new list by entering a new item. He
+		# is less intersting than Edith
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		# Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegexpMatches(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		#Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 
 		# Satisfied, she goes back to sleep
